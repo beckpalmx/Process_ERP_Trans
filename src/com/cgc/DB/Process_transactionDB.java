324f0b9c26aeee9e1af35dba289c65bc45652d8e
beckpalmx <beckpalmx@hotmail.com>
2015-12-21 14:10:00 +0700

    Update 21-12-58 1400
    Update 21-12-58 1400

--- src/com/cgc/DB/Process_transactionDB.java
+++ src/com/cgc/DB/Process_transactionDB.java
@@ -5,6 +5,7 @@
 package com.cgc.DB;
 
 import com.cgc.bean.DataBean_Transaction_Process;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,94 +14,115 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
+
 import com.cgc.Util.DateUtil;
 
 
 /**
- *
  * @author Beckeck
  */
 public class Process_transactionDB {
 
     private final DateUtil DT = new DateUtil();
 
+    private static int numrow(String SQL, Connection con) throws Exception {
+        ResultSet rs = null;
+        int numrow = 0;
+        try {
+            rs = con.createStatement().executeQuery(SQL);
+            while (rs.next()) {
+                numrow = rs.getInt(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            return numrow;
+        }
+    }
+
     public void generater_transaction_process(String date_from, String date_to, String process_id, String table, String doc_type, String r, String username) throws Exception {
         ArrayList<DataBean_Transaction_Process> obj_AL_process_transaction = new ArrayList<>();
-        Connection con = new DBConnect().openConnection_ERP_Y();
-        Connection con2 = new DBConnect().openConnection_ERP_Y();
-        ResultSet rs, rs1, rs2 ;
-        PreparedStatement p = null ;
+        //Connection con = new DBConnect().openConnection_ERP_Y();
+        //Connection con2 = new DBConnect().openConnection_ERP_Y();
+        //try (Connection con = new DBConnect().openConnection_ERP_Y()) {
+        try (Connection con = new DBConnect().openConnection_ERP_Y(); Connection con2 = new DBConnect().openConnection_ERP_Y()) {
 
-        //Random r = new Random();
-        String SQL_DEL, SQL_DEL1, SQL, SQL1, SQL2, token, SQL_TimeStamp;
+            ResultSet rs, rs1, rs2;
+            PreparedStatement p = null;
 
-        String SQL_MProduct, SQL_Last_Movement, set_last_movement = "";
-        String check = "";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            //Random r = new Random();
+            String SQL_DEL, SQL_DEL1, SQL, SQL1, SQL2, token, SQL_TimeStamp;
 
-        String Doc_date, Price_Month, Price_Year, Price_Per_Unit = "";
+            String SQL_MProduct, SQL_Last_Movement, set_last_movement = "";
+            String check = "";
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-        //String MonthString = "";
-        //int Record ;
+            String Doc_date, Price_Month, Price_Year, Price_Per_Unit = "";
 
-        //int count_loop = 0;
-        //System.out.println("Table : " + table);
-        //System.out.println("r : " + r);
-        //System.out.println("Date From Param Send : " + date_from);
-        //System.out.println("Date To Param Send : " + date_to);
-        System.out.println("Transaction Start Process process_id : " + process_id);
+            //String MonthString = "";
+            //int Record ;
 
-        //int max = 5000;
-        //int min = 1000;
+            //int count_loop = 0;
+            //System.out.println("Table : " + table);
+            //System.out.println("r : " + r);
+            //System.out.println("Date From Param Send : " + date_from);
+            //System.out.println("Date To Param Send : " + date_to);
+            System.out.println("Transaction Start Process process_id : " + process_id);
 
-        //Random rand = new Random();
+            //int max = 5000;
+            //int min = 1000;
 
-        //int randomNum = rand.nextInt((max - min) + 1) + min;
+            //Random rand = new Random();
 
-        //System.out.println(" randomNum = " + randomNum);
+            //int randomNum = rand.nextInt((max - min) + 1) + min;
 
-        //System.out.println("doc_type : " + doc_type);
-        SQL_DEL = " delete from t_transaction_stock_process where process_id = '" + process_id + "'";
-        SQL_DEL1 = " delete from t_transaction_stock_process_error where process_id = '" + process_id + "'";
+            //System.out.println(" randomNum = " + randomNum);
 
-        delete(SQL_DEL, con, p);
-        delete(SQL_DEL1, con, p);
-        switch (process_id) {
-            case "PR_115":
-            case "PR_117":
-                SQL = " select runno,doc_id,doc_date,line_no,doc_type,wh_product_id,wh_weight_final,wh_warehouse_id,wh_location_id,iodine,pgroup_id from " + table
-                        + " where delete_flag <> 'Y' and complete_flag = 'Y' "
-                        + " and to_date(format_date(doc_date),'YYYY-MM-DD') between to_date(format_date('" + date_from + "'),'YYYY-MM-DD') AND to_date(format_date('" + date_to + "'),'YYYY-MM-DD')";
-                break;
-            case "PR_060":
-                SQL = " select runno,doc_id,doc_date,line_no,doc_type,wh_product_id,wh_weight_final,wh_warehouse_id,wh_location_id,product_iodine as iodine,pgroup_id "
-                        + " from " + table
-                        + " where delete_flag <> 'Y' and complete_flag = 'Y' and (weight <> '0' and weight <> '') "
-                        + " and to_date(format_date(doc_date),'YYYY-MM-DD') between to_date(format_date('" + date_from + "'),'YYYY-MM-DD') AND to_date(format_date('" + date_to + "'),'YYYY-MM-DD')";
-                //System.out.println("60 SQL = " + SQL);
-                break;
-            default:
-                SQL = " select runno,doc_id,doc_date,line_no,wh_product_id,wh_weight_final,wh_warehouse_id,wh_location_id,iodine,pgroup_id from " + table
-                        + " where delete_flag <> 'Y' and complete_flag = 'Y' "
-                        + " and to_date(format_date(doc_date),'YYYY-MM-DD') between to_date(format_date('" + date_from + "'),'YYYY-MM-DD') AND to_date(format_date('" + date_to + "'),'YYYY-MM-DD')";
-                break;
-        }
+            //System.out.println("doc_type : " + doc_type);
+            SQL_DEL = " delete from t_transaction_stock_process where process_id = '" + process_id + "'";
+            SQL_DEL1 = " delete from t_transaction_stock_process_error where process_id = '" + process_id + "'";
 
-        SQL = SQL + " order by to_date(format_date(doc_date),'YYYY-MM-DD')";
-        //System.out.println("Main SQL = " + SQL);
+            delete(SQL_DEL, con, p);
+            delete(SQL_DEL1, con, p);
+            switch (process_id) {
+                case "PR_115":
+                case "PR_117":
+                    SQL = " select runno,doc_id,doc_date,line_no,doc_type,wh_product_id,wh_weight_final,wh_warehouse_id,wh_location_id,iodine,pgroup_id from " + table
+                            + " where delete_flag <> 'Y' and complete_flag = 'Y' "
+                            + " and to_date(format_date(doc_date),'YYYY-MM-DD') between to_date(format_date('" + date_from + "'),'YYYY-MM-DD') AND to_date(format_date('" + date_to + "'),'YYYY-MM-DD')";
+                    break;
+                case "PR_060":
+                    SQL = " select runno,doc_id,doc_date,line_no,doc_type,wh_product_id,wh_weight_final,wh_warehouse_id,wh_location_id,product_iodine as iodine,pgroup_id "
+                            + " from " + table
+                            + " where delete_flag <> 'Y' and complete_flag = 'Y' and (weight <> '0' and weight <> '') "
+                            + " and to_date(format_date(doc_date),'YYYY-MM-DD') between to_date(format_date('" + date_from + "'),'YYYY-MM-DD') AND to_date(format_date('" + date_to + "'),'YYYY-MM-DD')";
+                    //System.out.println("60 SQL = " + SQL);
+                    break;
+                default:
+                    SQL = " select runno,doc_id,doc_date,line_no,wh_product_id,wh_weight_final,wh_warehouse_id,wh_location_id,iodine,pgroup_id from " + table
+                            + " where delete_flag <> 'Y' and complete_flag = 'Y' "
+                            + " and to_date(format_date(doc_date),'YYYY-MM-DD') between to_date(format_date('" + date_from + "'),'YYYY-MM-DD') AND to_date(format_date('" + date_to + "'),'YYYY-MM-DD')";
+                    break;
+            }
 
-        SQL1 = " select count(*) from " + table
-                + " where delete_flag <> 'Y' "
-                + " and to_date(format_date(doc_date),'YYYY-MM-DD') between to_date(format_date('" + date_from + "'),'YYYY-MM-DD') AND to_date(format_date('" + date_to + "'),'YYYY-MM-DD')";
+            SQL = SQL + " order by to_date(format_date(doc_date),'YYYY-MM-DD')";
+            //System.out.println("Main SQL = " + SQL);
 
-        //Record = numrow(SQL1, con);
-        //System.out.println("SQL = " + SQL);
-        //System.out.println("Record = " + Record);
+            SQL1 = " select count(*) from " + table
+                    + " where delete_flag <> 'Y' "
+                    + " and to_date(format_date(doc_date),'YYYY-MM-DD') between to_date(format_date('" + date_from + "'),'YYYY-MM-DD') AND to_date(format_date('" + date_to + "'),'YYYY-MM-DD')";
 
-        //token = process_id + "_" + new SimpleDateFormat("ddMMyy_hhmmssS").format(new Date());
-        //SQL_TimeStamp = " Insert into t_process_log (log_id,process_id,start_time,create_by) values ('" + token + "','" + process_id + "','" + new Timestamp(new java.util.Date().getTime()) + "','" + username + "')";
-        //InsTimeStamp(SQL_TimeStamp, con, p);
-        //if (Record >= 1) {
+            //Record = numrow(SQL1, con);
+            //System.out.println("SQL = " + SQL);
+            //System.out.println("Record = " + Record);
+
+            //token = process_id + "_" + new SimpleDateFormat("ddMMyy_hhmmssS").format(new Date());
+            //SQL_TimeStamp = " Insert into t_process_log (log_id,process_id,start_time,create_by) values ('" + token + "','" + process_id + "','" + new Timestamp(new java.util.Date().getTime()) + "','" + username + "')";
+            //InsTimeStamp(SQL_TimeStamp, con, p);
+            //if (Record >= 1) {
 
             System.out.println("SQL = " + SQL);
 
@@ -143,15 +165,15 @@ public class Process_transactionDB {
 
                     Month = Integer.parseInt(Price_Month);
 
-                    /*                     
-                     Test Case 
+                    /*
+                     Test Case
 
                      if (rs.getString("wh_product_id").equals("C-I0801A-0+30-K") && process_id.equals("PR_060")) {
                      System.out.println("Case YYY = " + SQL2);
                      System.out.println("AAA Price_Year = " + Price_Year
                      + " Month = " + Month
                      + " doc_id = " + rs.getString("doc_id")
-                     + " iodine = " + rs.getString("iodine")                                
+                     + " iodine = " + rs.getString("iodine")
                      + " pgroup_id = " + rs.getString("pgroup_id"));
                      }
                      */
@@ -218,7 +240,7 @@ public class Process_transactionDB {
                 }
                 bean.setPrice_per_unit(Price_Per_Unit);
 
-                /*                 
+                /*
                  if (rs.getString("wh_product_id").equals("C-I0801A-0+30-K") && process_id.equals("PR_060")) {
                  System.out.println("XXX Price_Per_Unit = " + Price_Per_Unit);
                  }
@@ -241,7 +263,7 @@ public class Process_transactionDB {
 
                 obj_AL_process_transaction.add(bean);
 
-                if (rs.getString("wh_product_id")==(null) || rs.getString("wh_product_id").equals("") || rs.getString("doc_date")==(null) || rs.getString("doc_date").equals("")) {
+                if (rs.getString("wh_product_id") == (null) || rs.getString("wh_product_id").equals("") || rs.getString("doc_date") == (null) || rs.getString("doc_date").equals("")) {
                     insert_error(obj_AL_process_transaction, con, p);
                 } else {
                     insert(obj_AL_process_transaction, con, p);
@@ -280,7 +302,7 @@ public class Process_transactionDB {
                                     + " ,update_date = '" + new Timestamp(new java.util.Date().getTime()) + "'"
                                     + " where runno = " + rs2.getInt("runno");
 
-                            //System.out.println("SQL_Last_Movement = " + SQL_Last_Movement);                        
+                            //System.out.println("SQL_Last_Movement = " + SQL_Last_Movement);
                             Update_LastProductMovement(SQL_Last_Movement, con, p);
 
                             //System.out.println(rs2.getInt("runno") + " | " + rs2.getString("product_id") + " | " + set_last_movement + " Check = " + check);
@@ -293,32 +315,15 @@ public class Process_transactionDB {
                 //System.out.println("process_id = " + process_id + " | product_id = " + rs.getString("wh_product_id") + " count_loop = " + count_loop + " | " + "doc_date = " + rs.getString("doc_date"));
             }
 
-        //}
+            //}
 
-        //System.out.println("P1 count_loop = " + count_loop);
-        //SQL_TimeStamp = " Update t_process_log set condition = '" + SQL.replace("'", "#") + "', remark = '" + Record + " Record',complete_flag = 'Y' , end_time = '" + new Timestamp(new java.util.Date().getTime()) + "', update_by = '" + username + "' where log_id = '" + token + "'";
-        //System.out.println("SQL_TimeStamp = " + SQL_TimeStamp);
-        //InsTimeStamp(SQL_TimeStamp, con, p);
-        System.out.println("End Process process_id : " + process_id);
-
-    }
-
-    private static int numrow(String SQL, Connection con) throws Exception {
-        ResultSet rs = null;
-        int numrow = 0;
-        try {
-            rs = con.createStatement().executeQuery(SQL);
-            while (rs.next()) {
-                numrow = rs.getInt(1);
-            }
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            return numrow;
+            //System.out.println("P1 count_loop = " + count_loop);
+            //SQL_TimeStamp = " Update t_process_log set condition = '" + SQL.replace("'", "#") + "', remark = '" + Record + " Record',complete_flag = 'Y' , end_time = '" + new Timestamp(new java.util.Date().getTime()) + "', update_by = '" + username + "' where log_id = '" + token + "'";
+            //System.out.println("SQL_TimeStamp = " + SQL_TimeStamp);
+            //InsTimeStamp(SQL_TimeStamp, con, p);
+            System.out.println("End Process process_id : " + process_id);
         }
+
     }
 
     private void insert(ArrayList<DataBean_Transaction_Process> obj_AL, Connection con, PreparedStatement p) throws Exception {
